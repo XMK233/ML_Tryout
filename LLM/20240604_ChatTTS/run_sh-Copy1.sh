851e3a63fc168723228be573c3c6b9d3304b1a65
Minke Xiu <xmk233@163.com>
2024-12-21 00:28:30 +0800

    some updates

---
+++ LLM/20240604_ChatTTS/run_sh-Copy1.sh
@@ -0,0 +1,20 @@
+python test.py -since 400 -till 410
+sleep 3
+python test.py -since 410 -till 420
+sleep 3
+python test.py -since 420 -till 430
+sleep 3
+python test.py -since 430 -till 440
+sleep 3
+python test.py -since 440 -till 450
+sleep 3
+python test.py -since 450 -till 460
+sleep 3
+python test.py -since 460 -till 470
+sleep 3
+python test.py -since 470 -till 480
+sleep 3
+python test.py -since 480 -till 490
+sleep 3
+python test.py -since 490 -till 500
+sleep 3
---
+++ LLM/20240604_ChatTTS/run_sh.sh
@@ -0,0 +1,20 @@
+python test.py -since 300 -till 310
+sleep 3
+python test.py -since 310 -till 320
+sleep 3
+python test.py -since 320 -till 330
+sleep 3
+python test.py -since 330 -till 340
+sleep 3
+python test.py -since 340 -till 350
+sleep 3
+python test.py -since 350 -till 360
+sleep 3
+python test.py -since 360 -till 370
+sleep 3
+python test.py -since 370 -till 380
+sleep 3
+python test.py -since 380 -till 390
+sleep 3
+python test.py -since 390 -till 400
+sleep 3
