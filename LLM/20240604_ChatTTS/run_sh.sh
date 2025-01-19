d1fda0f85d5b879f2d56943c7ce9579e4a1498ba
Minke Xiu <xmk233@163.com>
2025-01-19 18:55:50 +0800

    some updates
    大明边防 那本书已经全部转换完了。

--- LLM/20240604_ChatTTS/run_sh.sh
+++ LLM/20240604_ChatTTS/run_sh.sh
@@ -1,20 +1,61 @@
-python test.py -since 300 -till 310
+# python test.py -since 1100 -till 1110
+# sleep 3
+# python test.py -since 1110 -till 1120
+# sleep 3
+# python test.py -since 1120 -till 1130
+# sleep 3
+# python test.py -since 1130 -till 1140
+# sleep 3
+# python test.py -since 1140 -till 1150
+# sleep 3
+# python test.py -since 1150 -till 1160
+# sleep 3
+# python test.py -since 1160 -till 1170
+# sleep 3
+# python test.py -since 1170 -till 1180
+# sleep 3
+# python test.py -since 1180 -till 1190
+# sleep 3
+# python test.py -since 1190 -till 1200
+# sleep 3
+# python test.py -since 1200 -till 1210
+# sleep 3
+# python test.py -since 1210 -till 1220
+# sleep 3
+# python test.py -since 1220 -till 1230
+# sleep 3
+# python test.py -since 1230 -till 1240
+# sleep 3
+# python test.py -since 1240 -till 1250
+# sleep 3
+
+python test.py -since 1250 -till 1260
 sleep 3
-python test.py -since 310 -till 320
+python test.py -since 1260 -till 1270
 sleep 3
-python test.py -since 320 -till 330
+python test.py -since 1270 -till 1280
 sleep 3
-python test.py -since 330 -till 340
+python test.py -since 1280 -till 1290
 sleep 3
-python test.py -since 340 -till 350
+python test.py -since 1290 -till 1300
 sleep 3
-python test.py -since 350 -till 360
+python test.py -since 1300 -till 1310
 sleep 3
-python test.py -since 360 -till 370
+python test.py -since 1310 -till 1320
 sleep 3
-python test.py -since 370 -till 380
+python test.py -since 1320 -till 1330
 sleep 3
-python test.py -since 380 -till 390
+python test.py -since 1330 -till 1340
 sleep 3
-python test.py -since 390 -till 400
+python test.py -since 1340 -till 1350
+sleep 3
+python test.py -since 1350 -till 1360
+sleep 3
+python test.py -since 1360 -till 1370
+sleep 3
+python test.py -since 1370 -till 1380
+sleep 3
+python test.py -since 1380 -till 1390
+sleep 3
+python test.py -since 1390 -till 1400
 sleep 3
